343929d61c5e4371b71625ad14ba90d3377f0a19
Denis Minzakirov <denis.minzakirov@intexsoft.by>
2021-05-30 10:27:23 +0300

    feat: add resolv function

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -1,9 +1,20 @@
 package subtask1
+import kotlin.collections.*
 
 class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        return if (sadArray.size <= 2) { sadArray } else {
+            val result = sadArray.toMutableList()
+            var i: Int = 1
+            while (i < result.size - 1) {
+                if (result[i] > result[i - 1] + result[i + 1]) {
+                    result.removeAt(i)
+                    if (i > 1) {i--}
+                } else {i++}
+            }
+            return result.toIntArray()
+        }
     }
 }
--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -5,6 +5,18 @@ class BillCounter {
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+        var price = 0.0f
+
+        for (i in bill.indices)
+        {
+            if(i != k) {
+                price += bill[i]/2.0F
+            }
+        }
+        return if((price - b).toInt() == 0) {
+            "Bon Appetit"
+        } else {
+            (b - price).toInt().toString()
+        }
     }
 }
